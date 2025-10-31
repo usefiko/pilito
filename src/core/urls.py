07661f91534c6e2b45ccf77b85ad21df62d88dfa
Fan Wang <nimadorostkar97@gmail.com>
2025-10-31 15:58:30 +0330

    pusssh

--- src/core/urls.py
+++ src/core/urls.py
@@ -33,7 +33,6 @@ schema_view = get_schema_view(
     generator_class=CustomOpenAPISchemaGenerator,
 )
 
-
 urlpatterns = [
     path('docs/', schema_view.with_ui('swagger',cache_timeout=0),name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
